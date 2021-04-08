b348c76e2cfec3c096d4d1d422b83b3854bd5fc6
arturkolaska <kolaska128@gmail.com>
2021-04-08 09:09:31 +0200

    Add HelloController

---
+++ src/main/java/pl/allegrotech/functionalprogramming/HelloController.java
@@ -0,0 +1,16 @@
+package pl.allegrotech.functionalprogramming;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(
+            @RequestParam(value = "name", defaultValue = "World") String name
+    ) {
+        return String.format("Hello %s!", name);
+    }
+}
